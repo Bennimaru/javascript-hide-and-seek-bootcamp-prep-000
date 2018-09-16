e3d5c4a69a21296cf9a5176089b1232b85b92317
Benson Yu <bennywhyu@gmail.com>
2018-09-16 04:07:44 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-
+function getFirstSelector(selector){
+  return document.querySelector("selector")
+}
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target")
+}
+function increaseRankBy(n){
+  var lis = document.getElementById("app").querySelectorAll("ul.ranked-list li")
+  for(let i=0;i<lis.length;i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML)+n
+  }
+}
+function deepestChild(){
+  var lis = document.getElementById("grand-node").querySelectorAll("div");
+  var deepestNode;
+  for (let i=0;i<lis.length;i++){
+    deepestNode=lis[i].querySelector("div")
+  }
+  return deepestNode;
+}
